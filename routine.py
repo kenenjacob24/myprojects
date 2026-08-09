1310113271d31c94bd4426ac16a5cf30cadfc0c5
kenenjacob24 <kenenjacob24@gmail.com>
2026-08-09 16:41:32 -0400

    Merge branch 'master' of https://github.com/kenenjacob24/myprojects
    # Conflicts:
    #	routine.py

--- routine.py
+++ routine.py
@@ -1,10 +1,17 @@
+# Print the title
 print("My Daily Routine")
 
 #step 1: Waking up
 print("I wake up at 7 AM.")
 
-#step 3 testing
+#step 2 testing
+print("I go to school at 8AM.")
 
-#step 4
+#step 3: Playing after school
+print("I am playing with my friends in the evening.")
 
-#Step 5
+#Step 4: Studying
+print("I study for two hours at night")
+
+# Calculating total study time in a week
+print("Total study hours in a week:", 2 * 7)
