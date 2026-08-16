eafed45a8ddbe85c6b54735ea69c88407ba8e9fd
kenenjacob24 <kenenjacob24@gmail.com>
2026-08-16 12:58:39 -0400

    Add files via upload

---
+++ Codingal classes/M1 lesson 4/average.py
@@ -0,0 +1,13 @@
+Tree1 = 98
+Tree2 = 94
+Tree3 = 41
+Tree4 = 95
+Tree5 = 11
+
+# Find the total of trees
+sum = Tree1+Tree2+Tree3+Tree4+Tree5
+print("the sum of all the 5 trees is:", sum)
+
+# find the average of trees
+average = sum/5
+print("the average of all the tree is :", average)
