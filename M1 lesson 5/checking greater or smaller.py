ba1ff6e95ff1dcfa7087855252da4dd703df8e8d
kenenjacob24 <kenenjacob24@gmail.com>
2026-08-19 18:44:38 -0400

    Add files via upload

---
+++ M1 lesson 5/checking greater or smaller.py
@@ -0,0 +1,8 @@
+i = int(input("enter a number: "))
+if i < 15:
+    print(i, "is smaller than 15")
+    print("I am in if block")
+else:
+    print(i, "is not smaller than 15")
+    print("I am in else block")
+print("I am not under if nor else block")
