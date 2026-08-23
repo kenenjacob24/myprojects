e1d80b5e5601b56d729f977896c9962a0782e029
kenenjacob24 <kenenjacob24@gmail.com>
2026-08-23 13:51:56 -0400

    Add files via upload

---
+++ M1 lesson 5/coditional statement.py
@@ -0,0 +1,8 @@
+temperature = int(input("Enter the temperature: "))
+
+if temperature >=70:
+    print("wear a light clothes")
+elif temperature >= 50:
+    print("wear a jacket")
+else:
+    print("wear a heavy jacket")
