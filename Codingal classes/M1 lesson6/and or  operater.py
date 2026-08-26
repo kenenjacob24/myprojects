ac669c08a62305c7074bbedfaec31334f0771a97
kenenjacob24 <kenenjacob24@gmail.com>
2026-08-26 17:17:27 -0400

    Add files via upload

---
+++ Codingal classes/M1 lesson6/and or  operater.py
@@ -0,0 +1,25 @@
+a = 13
+b = 15
+c = 0
+
+if a and b and c:
+    print("All the number have boolean valuer as true")
+else:
+    print("Atleast one number has boolean value as false")
+
+
+
+a = 20
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("Either of the number is greater than o")
+else:
+    print("No number is greater than 0")
+
+if b > 0 or c > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+    
---
+++ Codingal classes/M1 lesson6/character.py
@@ -0,0 +1,8 @@
+character = input("Enter a character: ")
+
+if ('A'<= character <= 'Z') or ('a' <= character <= 'z'):
+    print(character, "is an alphabet.")
+else:
+    print(character, "is not an alphabet.")
+
+    
