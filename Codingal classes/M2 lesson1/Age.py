8109d71cf667bc1842506ce7b6e549f841e1e404
kenenjacob24 <kenenjacob24@gmail.com>
2026-08-29 15:24:30 -0400

    Add files via upload

---
+++ Codingal classes/M2 lesson1/Age.py
@@ -0,0 +1,8 @@
+Age = int(input("please enter your age: "))
+if(Age>10):
+    if(Age<20):
+        print("you can inrole in this class")
+    else:
+        print("you cannot inrole in this class")
+else:
+    print("you cannot inrole in this class")
