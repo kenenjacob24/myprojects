4ee7faefb052614ebe6c4004079b4452d2df29f4
kenenjacob24 <kenenjacob24@gmail.com>
2026-09-07 13:05:16 -0400

    Add files via upload

---
+++ Codingal classes/M2 lesson4/Character.py
@@ -0,0 +1,13 @@
+string = input("Please enter your own word: ")
+
+char = input("Please enter your own character: ")
+
+i = 0
+count = 0
+while i < len(string):
+
+    if string[i] == char:
+        count += 1
+    i = i + 1
+
+print("the total Number of Times ",char, "has Occurred =", count)
